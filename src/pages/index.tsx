fd9c1a89972a14fd6b14750a277df873b211f6b4
sonegobruno <sonegobruno@gmail.com>
2022-09-21 15:55:21 -0300

    feat: update bg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ import { ButtonGeneratorPDF } from "@/components/ButtonGeneratorPDF";
 export default function Home() {
 
   return (
-    <Flex align="center" justify="center" minW="800px" overflowX="auto">
+    <Flex align="center" bg="gray.50" justify="center" minW="800px" overflowX="auto">
 
       <ButtonGeneratorPDF 
         position="fixed"
